f6ce6d62b22457881dd248aa3c10e77b2088b0e9
Niyaz Valeev <niaz-valeev@mail.ru>
2023-03-09 15:16:21 +0300

    feat: реализованы методы add, get класса SqliteRepository

--- bookkeeper/repository/sqlite_repository.py
+++ bookkeeper/repository/sqlite_repository.py
@@ -2,22 +2,65 @@ import sqlite3
 
 from itertools import count
 from typing import Any
-
+from inspect import get_annotations
 from bookkeeper.repository.abstract_repository import AbstractRepository, T
+from bookkeeper.models.category import Category
+from bookkeeper.models.expense import Expense
+from bookkeeper.models.budget import Budget
 
 
 class SqliteRepository(AbstractRepository[T]):
-    def __init__(self) -> None:
-        self._counter = count(1)
+    def __init__(self, db_file: str, cls: type) -> None:
+        self.db_file = db_file
+        self.table_name = cls.__name__.lower()
+        self.fields = get_annotations(cls, eval_str=True)
+        self.fields.pop('pk')
 
     def add(self, obj: T) -> int:
-        return 0
+        names = ', '.join(self.fields.keys())
+        placeholders = ', '.join("?" * len(self.fields))
+        values = [getattr(obj, x) for x in self.fields]
+        print(names)
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            cur.execute('PRAGMA foreign_keys = ON')
+            cur.execute(
+                f'INSERT INTO {self.table_name} ({names}) VALUES ({placeholders})',
+                values
+            )
+            obj.pk = cur.lastrowid
+        con.close()
+        return obj.pk
 
     def get(self, pk: int) -> T | None:
         return
 
     def get_all(self, where: dict[str, Any] | None = None) -> list[T]:
-        return None
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            cur.execute('PRAGMA foreign_keys = ON')
+            cur.execute(f'SELECT * FROM {self.table_name}')
+            rows = cur.fetchall()
+        con.close()
+        return rows
 
     def delete(self, pk: int) -> None:
         return
+
+    def update(self, obj: T) -> None:
+        return
+
+
+def repository_factory():
+    return {
+        Expense: SqliteRepository('test.sqlite', Expense),
+        Category: SqliteRepository('test.sqlite', Category),
+        Budget: SqliteRepository('test.sqlite', Budget)
+    }
+
+
+r = SqliteRepository('test.sqlite', Expense)
+o = Expense(1, 1)
+o1 = Expense(amount=3, category=2)
+print(r.add(o))
+#print(r.get_all())
