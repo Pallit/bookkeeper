22573a53b819e45f007b26c04ebc5e9b06b0bc26
Niyaz Valeev <niaz-valeev@mail.ru>
2023-03-12 18:56:29 +0300

    docs: добавлена документация для Budget

--- bookkeeper/models/budget.py
+++ bookkeeper/models/budget.py
@@ -1,9 +1,19 @@
+"""
+Модель бюджета
+"""
+
 from dataclasses import dataclass
 
 
 @dataclass(slots=True)
 class Budget:
-
+    """
+    Бюджет.
+    period - временной период
+    amount - кол-во затраченных средств
+    budget - выделенный бюджет
+    pk - id записи в базе данных
+    """
     period: str
     amount: int
     budget: int
