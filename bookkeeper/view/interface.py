dd854bdc67c762e83e7638745948396713dd56fe
Niyaz Valeev <niaz-valeev@mail.ru>
2023-03-13 20:34:43 +0300

    refactor and style: убраны лишние print() и убраны лишние комментирования

--- bookkeeper/view/interface.py
+++ bookkeeper/view/interface.py
@@ -68,7 +68,6 @@ class CategoryRedactor(QtWidgets.QWidget):
         if ind == -1:
             return
         Presenter.delete_category(self.indeces[ind])
-        print(ind, self.indeces[ind])
         self.indeces.pop(ind)
         data_category = Presenter.get_category_data()
         self.list.clear()
@@ -84,9 +83,6 @@ class TableModel(QtCore.QAbstractTableModel):
 
     def data(self, index, role):
         if role == Qt.DisplayRole:
-            # See below for the nested-list data structure.
-            # .row() indexes into the outer list,
-            # .column() indexes into the sub-list
             return self._data[index.row()][index.column()]
 
     def rowCount(self, index):
@@ -94,8 +90,6 @@ class TableModel(QtCore.QAbstractTableModel):
         return len(self._data)
 
     def columnCount(self, index):
-        # The following takes the first sub-list, and returns
-        # the length (only works if all rows are an equal length)
         if len(self._data) == 0:
             return 0
         return len(self._data[0])
@@ -240,7 +234,6 @@ class ExpenseTable(QtWidgets.QWidget):
         """
         Сбрасывает все данные
         """
-        print("debug")
         Presenter.clear_data()
         data_expense, labels_expense = Presenter.get_expense_data()
         self.table.setModel(TableModel(data_expense, labels_expense))
